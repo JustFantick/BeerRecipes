06af30ce35b1906cc4ed7beec61662521b54c1e6
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-07-13 17:34:49 +0300

    Received server`s data into state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,32 @@
-import React, { useState } from 'react';
-import background from "../img/bg1.webp"
-import Main from "./main/main.jsx";
+import React, { useEffect, useState } from 'react';
 
 export default function App() {
+	const [serverData, setServerData] = useState([]);
+
+	useEffect(() => {
+		async function fetchData() {
+			setServerData([]);
+
+			const response = await fetch(`https://api.punkapi.com/v2/beers?page=${1}`);
+			const data = await response.json();
+
+			if (!ignore) {
+				console.log(data);
+				setServerData(await data);
+			};
+		}
+
+		let ignore = false;
+		fetchData();
+
+		return () => {
+			ignore = true;
+		}
+	}, []);
+
 	return (
-		<div className="wrapper" style={{ backgroundImage: `url(${background})` }}>
-			Beer recipes
+		<div className="wrapper">
+
 		</div>
 	)
 }
