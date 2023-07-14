77928c95f185a0e9dc5b438c6722502743c2d94b
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-07-14 17:40:55 +0300

    Removed white spaces

--- src/store/store.js
+++ src/store/store.js
@@ -5,11 +5,9 @@ const store = (set, get) => ({
 	fetch: async () => {
 		const response = await fetch(`https://api.punkapi.com/v2/beers?page=${1}`);
 		set({ recipes: await response.json() });
-		console.log(get().recipes[1]);
 	},
 	increasePopulation: () => set((state) => ({ recipes: state.recipes + 1 })),
 	setNull: () => set({ recipes: 0 }),
-
 });
 
 export const useRecipesStore = create(store);
