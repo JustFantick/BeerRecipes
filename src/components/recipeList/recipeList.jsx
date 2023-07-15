ec7d6829ffa63f054adb5ed89f15ad205e9508ff
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-07-15 09:22:50 +0300

    Removed unnecessary code, render list limited 15

--- src/components/recipeList/recipeList.jsx
+++ src/components/recipeList/recipeList.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import { useRecipesStore } from '../../store/store.js';
 import RecipeCart from '../recipeCart/recipeCart.jsx';
-import { useEffect } from 'react';
 
 export default function RecipeList() {
 	const recipes = useRecipesStore((state) => state.recipes);
 
-	useEffect(() => {
-		//console.log(recipes);
-	}, [recipes]);
-
-	const arr = [{ name: 'jack' }, { name: 'two' }, { name: 'three' }]
-
 	return (
 		<main className="recipe-list">
-			{recipes.map(recipe =>
+			{recipes.slice(0, 15).map(recipe =>
 				<RecipeCart key={recipe.id} id={recipe.id}
 					imgUrl={recipe.image_url} imgAlt={recipe.name + '_img'}
 					cartName={recipe.name}
