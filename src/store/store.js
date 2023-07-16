857b0c0c04c318c61413d3938c55d388cf14ad6a
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-07-16 11:59:49 +0300

    Added vars and code changes to provide anims

--- src/store/store.js
+++ src/store/store.js
@@ -5,6 +5,7 @@ const store = (set, get) => ({
 	fetchCounter: 1,
 	recipeId: 1,
 	renderRecipeList: true,
+	selectionCartMode: false,
 	fetchServersData: async () => {
 		const response = await fetch(`https://api.punkapi.com/v2/beers?page=${get().fetchCounter}`);
 		set((state) => ({ fetchCounter: state.fetchCounter + 1 }));
@@ -14,9 +15,13 @@ const store = (set, get) => ({
 		set((state) => ({
 			recipes: state.recipes.map((obj, i) => {
 				if (id === obj.id) {
-					if (!Object.hasOwn(obj, 'chosen')) {
+					if (!Object.hasOwn(obj, 'chosen') || obj.chosen === false) {
+						state.setSelectionCartMode(true);
 						return { ...obj, chosen: true };
 					} else {
+						if (state.recipes.slice(0, 15).filter(recipe => !recipe.chosen || recipe.chosen === false).length === 14) {
+							state.setSelectionCartMode(false);
+						}
 						return { ...obj, chosen: !obj.chosen };
 					}
 				}
@@ -25,12 +30,18 @@ const store = (set, get) => ({
 			})
 		}));
 	},
+	setSelectionCartMode: (boolean) => set({ selectionCartMode: boolean }),
 	removeMarkedRecipes: () => {
 		set((state) => ({ recipes: state.recipes.filter(recipe => recipe.chosen !== true) }));
+		get().setSelectionCartMode(false);
 		if (get().recipes.length < 15) get().fetchServersData();
 	},
 	setRecipeId: (id) => set({ recipeId: id }),
 	setRenderRecipeList: (boolean) => set({ renderRecipeList: boolean }),
+	recipeListInAnim: false,
+	recipePageInAnim: false,
+	setRecipeListInAnim: (boolean) => set({ recipeListInAnim: boolean }),
+	setRecipePageInAnim: (boolean) => set({ recipePageInAnim: boolean }),
 });
 
 export const useRecipesStore = create(store);
