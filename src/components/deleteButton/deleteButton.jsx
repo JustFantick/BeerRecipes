d5700909bcd15bdf6c65765aaa77691ccd29a7ce
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-07-17 19:25:01 +0300

    Removed unnecessary code

--- src/components/deleteButton/deleteButton.jsx
+++ src/components/deleteButton/deleteButton.jsx
@@ -3,14 +3,13 @@ import { useRef } from 'react';
 import { CSSTransition } from 'react-transition-group';
 
 export default function DeleteButton(props) {
-	const buttonBlock = useRef(null);
 	return (
 		<CSSTransition
 			in={props.in} timeout={300}
 			classNames='delete-btn'
 			mountOnEnter unmountOnExit
 		>
-			<div className='delete-btn' onClick={props.onClick}></div>
+			<button className='delete-btn' onClick={props.onClick}></button>
 		</CSSTransition>
 	)
 }
